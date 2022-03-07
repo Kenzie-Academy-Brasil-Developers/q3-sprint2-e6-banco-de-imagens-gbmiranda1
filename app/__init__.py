53e3813026df626c025df0ab840f9645f9bf3bef
gbmiranda1 <gabrielamiranda@alu.ufc.br>
2022-03-07 05:02:55 -0300

    feat: arquivos

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,61 @@
 # Desenvolva suas rotas aqui
+from flask import Flask, jsonify, request, send_file, safe_join
+import os
+import zipfile
+from app.kenzie.image import getFiles, getFilesExtension, getFileDownload, upload_video
+
+
+app = Flask(__name__)
+maxLenght = os.getenv("MAX_CONTENT_LENGTH")
+
+
+file = os.getenv("FILES_DIRECTORY")
+extensions = os.getenv("ALLOWED_EXTENSIONS")
+direc = os.listdir(file)
+
+@app.get("/files")
+def getFilesRoute():
+    return jsonify({"files": getFiles()}), 200
+
+@app.get("/files/<extension>")
+def getFilesExtenstionRoute(extension):
+    if extension in extensions.split(","):
+        if(len(getFilesExtension(extension)) > 0 ):
+            return jsonify({"files": getFilesExtension(extension)}), 200
+        return {"message": "No files found in this extension."}, 404
+    return {"message": "We don't recognize this extension"}, 404
+
+@app.get("/download/<arquivo>")
+def getDownloadFile(arquivo):
+    return getFileDownload(arquivo)
+
+
+@app.get("/download-zip")
+def getDownloadZip():
+    extension = request.args["files_extension"]
+    compression = request.args["compression_ratio"]
+
+    inputfile = os.path.join(file, extension)
+    pathOutput = os.path.join("/tmp", f"{extension}.zip")
+
+    if extension in extensions.split(","):
+        if os.path.isfile(pathOutput):
+            os.remove(pathOutput)
+        if os.listdir(inputfile) == []:
+            return {"message": "empty"}, 404
+        os.system(f"zip -r -j -{compression} {pathOutput} {inputfile}")
+        return send_file(pathOutput, as_attachment=True)
+    return {"message": "we don't recognize this extension"}, 404
+
+@app.post("/upload")
+def postFile():
+    files = request.files
+    for filesIN in files.values():
+        try:
+            return upload_video(filesIN)
+        except FileExistsError:
+            return {"msg": "File already exists"}, 409
+        except FileNotFoundError:
+            return {'msg': "We only accept jpg, png and gif extension"}, 415
+        except Exception:
+            return {'msg': "Very large file"}, 413
--- app/kenzie/image.py
+++ app/kenzie/image.py
@@ -1,1 +1,64 @@
-# Desenvolva sua lógica de manipulação das imagens aqui
+from posixpath import abspath
+from flask import jsonify,send_file, safe_join
+import os
+from pathlib import Path 
+
+files_direc = os.getenv("FILES_DIRECTORY")
+direc = os.listdir(files_direc)
+extensions = os.getenv("ALLOWED_EXTENSIONS") 
+
+def getFiles():
+    files = []
+    for f in direc:
+        if os.listdir(f"img/{f}") != []:
+            files.append(os.listdir(f"img/{f}"))
+    return files
+
+def getFilesExtension(ext):
+    files = []
+    for f in direc:
+        if f.split(".")[-1] == ext:
+            files.append(os.listdir(f"img/{f}"))
+    return files
+
+def getFileDownload(arquivo):
+    
+    for dire in direc:
+        path = os.listdir(f"img/{dire}")
+        abspath = os.path.abspath(files_direc)
+        for files in path:
+            if files == arquivo:
+                return send_file(safe_join(f"{abspath}/{dire}/{arquivo}"), as_attachment=True), 200
+    return {"message": "No files found"}, 404
+
+def upload_video(file):
+    extension = file.filename.split(".")[-1]
+    if not(extension in direc) and extension in extensions.split(","):
+        os.system(f"mkdir img/{extension}")
+
+    filepath = get_file_path(f"{extension}/{file.filename}")
+
+    if fileExists(file.filename, extension):
+        raise FileExistsError
+    if not (extensionSuport(extension)):
+        raise FileNotFoundError
+    
+    file.save(filepath)
+    if Path(filepath).stat().st_size > 1048576:
+        os.system(f"rm img/{extension}/{file.filename}")
+        raise Exception
+    return {"msg": "videos uploaded"}, 201
+
+
+
+def get_file_path(filename: str):
+    abs_path = os.path.abspath(files_direc)
+    filepath = safe_join(abs_path, filename)
+
+    return filepath
+
+def fileExists(filename, extension):
+    return filename in os.listdir(os.path.join(files_direc, extension))
+
+def extensionSuport(extension):
+    return extension in extensions.split(",")
